86ff8513642a270e070eeb5978c8b949382acb9a
mustapha zakari <mzakari31@gmail.com>
2021-03-08 12:54:38 +0100

    add struct & plot

--- src/SPRING.jl
+++ src/SPRING.jl
@@ -1,3 +1,18 @@
 module SPRING
+using Plots
+    toto(x) = cos(x)
 
+    struct MyStruct
+        myfield::Float64
+    end
+
+    function go()
+        x=[Float64(i) for i ∈ 1:40]
+        y = toto.(x)
+        plot(x,y)
+
+        mf=MyStruct(3)
+        @show mf
+
+    end
 end # module
